73f7689b8929dfd29bd8cf4123ff0fe3eb087a01
Vikas Singh Mann <vickymann9774@gmail.com>
2024-04-18 23:01:28 -0600

    make cart page responsive.

--- Client/src/pages/Cart/index.jsx
+++ Client/src/pages/Cart/index.jsx
@@ -77,15 +77,15 @@ export default function CartPage() {
                 ))}              
               </>
             </div>
-      <div className="flex flex-col max-w-[40rem] px-3 pb-3">
-        <div className="border-2 px-3 pb-2 bg-[#171717]">
+      <div className="flex flex-col w-full sm:max-w-[40rem] px-3 pb-3">
+        <div className="border-2 px-2 pb-2 bg-black">
           <div className="border-b-4 py-8">
             <Heading className="text-center">Order Summary</Heading>
           </div>
-          <div className="flex flex-col gap-3 px-8 border-b-4 py-12">
+          <div className="flex flex-col gap-3 px-3 border-b-4 py-12">
             <div className="flex justify-between">
               <Text className="">Subtotal</Text>
-              <Text className="w-10">{totalPrice}</Text>
+              <Text className="w-10 text-center">{totalPrice}</Text>
             </div>
             <div className="flex justify-between">
               <Text className="">Shipping</Text>
@@ -96,13 +96,13 @@ export default function CartPage() {
               <Text className="w-8 text-center">Free</Text>
             </div>
           </div>
-          <div className="px-8 py-7">
+          <div className="px-3 py-7">
             <Text className="text-center flex justify-between">Total <span>{totalPrice}</span></Text>
           </div>
           {show ? 
-          <Link to="/checkout" className="w-full py-4 bg-green-500  font-semibold text-center text-black">Checkout</Link>
+          <Link to="/checkout" className="w-full py-4 bg-green-500  font-semibold text-center">Checkout</Link>
           : null}
-          <Link to="/productlist" className="w-full py-4 bg-red-500 font-semibold mt-4 text-center text-black">Continue Shopping</Link>
+          <Link to="/productlist" className="w-full py-4 bg-red-500 font-semibold mt-4 text-center">Continue Shopping</Link>
         </div>
       </div>
         </div>
